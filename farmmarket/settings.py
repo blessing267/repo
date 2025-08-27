8f70025f3002e533abba8785913f336b44e1d6e5
Blessing1-dev <c4046182@hallam.shu.ac.uk>
2025-08-27 15:53:57 +0100

    Include SQLite database and update settings for demo

--- farmmarket/settings.py
+++ farmmarket/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'farmmarket.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/home', 'site', 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
